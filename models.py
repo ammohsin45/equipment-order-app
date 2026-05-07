4cca913363246ed5aec4ae6fd5ac11872d142c79
amohsin45 <afia.mohsin@bakerhughes.com>
2026-05-07 09:40:35 -0500

    update models and schemas

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date
 from database import Base
 
+
 class OrderRecord(Base):
     __tablename__ = "orders"
 
--- schemas.py
+++ schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import date
 
+
 class OrderCreate(BaseModel):
     Equipment: str
     Order: str
